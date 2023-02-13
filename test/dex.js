36926b4d80b0e60de45eed99f8e2ae17ad4dd433
NehemieMbg <nehemie.mbg@gmail.com>
2023-02-13 23:34:15 +0100

    contracts tests

---
+++ test/dex.js
@@ -0,0 +1,73 @@
+const Dex = artifacts.require("Dex");
+const Emerys = artifacts.require("Emerys");
+const truffleAssert = require("truffle-assertions");
+
+contract("Dex", (accounts) => {
+  it("should throw an error if Eth balance is too low when creating BUY limit order", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await truffleAssert.reverts(
+      dex.createLimitOrder(0, web3.utils.fromUtf8("EMRS"), 10, 1)
+    );
+    dex.depositEth({ value: 10 });
+    await truffleAssert.passes(
+      dex.createLimitOrder(0, web3.utils.fromUtf8("EMRS"), 10, 1)
+    );
+  });
+
+  it("should throw an error if token balance is too low when creating SELL order", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await truffleAssert.reverts(
+      dex.createLimitOrder(1, web3.utils.fromUtf8("EMRS"), 10, 1)
+    );
+    await emerys.approve(dex.address, 500);
+    await dex.addToken(web3.utils.fromUtf8("EMRS"), emerys.address, {
+      from: accounts[0],
+    });
+    await dex.deposit(10, web3.utils.fromUtf8("EMRS"));
+    await truffleAssert.passes(
+      dex.createLimitOrder(1, web3.utils.fromUtf8("EMRS"), 10, 1)
+    );
+  });
+
+  it("The BUY order book should be ordered on price from highest to lowest starting at index 0", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await emerys.approve(dex.address, 500);
+    await dex.depositEth({ value: 3000 });
+    await dex.createLimitOrder(0, web3.utils.fromUtf8("EMRS"), 1, 200);
+    await dex.createLimitOrder(0, web3.utils.fromUtf8("EMRS"), 1, 100);
+    await dex.createLimitOrder(0, web3.utils.fromUtf8("EMRS"), 1, 300);
+
+    let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("EMRS"), 0);
+    assert(orderbook.length > 0);
+
+    for (let i = 0; i < orderbook.length - 1; i++) {
+      assert(
+        orderbook[i].price >= orderbook[i + 1].price,
+        "not right order in buy book"
+      );
+    }
+  });
+
+  it("The SELL order book should be ordered on price from lowest to highest starting at index 0", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await emerys.approve(dex.address, 500);
+    await dex.createLimitOrder(1, web3.utils.fromUtf8("EMRS"), 1, 200);
+    await dex.createLimitOrder(1, web3.utils.fromUtf8("EMRS"), 1, 100);
+    await dex.createLimitOrder(1, web3.utils.fromUtf8("EMRS"), 1, 300);
+
+    let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("EMRS"), 1);
+    console.log(orderbook);
+    for (let i = 0; i < orderbook.length - 1; i++) {
+      assert(
+        orderbook[i].price <= orderbook[i + 1].price,
+        "not right order in sell book"
+      );
+    }
+  });
+});
+
+// The BUY order book should be ordered on price from highest to lowest starting at index 0
---
+++ test/wallet.js
@@ -0,0 +1,43 @@
+const Dex = artifacts.require("Dex");
+const Emerys = artifacts.require("Emerys");
+const truffleAssert = require("truffle-assertions");
+
+contract("Dex", (accounts) => {
+  it("should only be possible for owner to add tokens", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await truffleAssert.passes(
+      dex.addToken(web3.utils.fromUtf8("EMRS"), emerys.address, {
+        from: accounts[0],
+      })
+    );
+
+    await truffleAssert.reverts(
+      dex.addToken(web3.utils.fromUtf8("EMRS"), emerys.address, {
+        from: accounts[1],
+      })
+    );
+  });
+
+  it("should handle deposits", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await emerys.approve(dex.address, 500);
+    await dex.deposit(100, web3.utils.fromUtf8("EMRS"));
+
+    let balance = await dex.balances(accounts[0], web3.utils.fromUtf8("EMRS"));
+    assert.equal(balance.toNumber(), 100);
+  });
+
+  it("should handle withdrawals", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await truffleAssert.passes(dex.withdraw(50, web3.utils.fromUtf8("EMRS")));
+  });
+
+  it("should handle faulty withdrawals", async () => {
+    let dex = await Dex.deployed();
+    let emerys = await Emerys.deployed();
+    await truffleAssert.reverts(dex.withdraw(500, web3.utils.fromUtf8("EMRS")));
+  });
+});
